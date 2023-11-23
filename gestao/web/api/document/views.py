0b13989f59871e466b1ff092a54e91984f2681bb
Eduard0803 <eduardo83204gmail.com>
2023-11-23 13:14:12 -0300

    add error handling in route 'document/affiliation'

--- gestao/web/api/document/views.py
+++ gestao/web/api/document/views.py
@@ -9,12 +9,16 @@ router = APIRouter()
 
 @router.get("/affiliation/{user_id}")
 async def get_user_affiliation(user_id: str) -> None:
-    user = await User.objects.get(id=user_id)
-    file_stream = generate_affiliation_file(user)
-    pdf_file_stream = convert_file(file_stream)
-    return StreamingResponse(
-        pdf_file_stream,
-        media_type=(
-            "application/pdf"
-        ),
-    )
+    try:
+        user = await User.objects.get(id=user_id)
+        file_stream = generate_affiliation_file(user)
+        pdf_file_stream = convert_file(file_stream)
+        return StreamingResponse(
+            pdf_file_stream,
+            media_type=(
+                "application/pdf"
+            ),
+        )
+    except Exception:
+        logging.error("Error occurred while get user", exc_info=True)
+        raise HTTPException(status_code=404, detail="User not found")
