8aec311d9f9f402003782a0667e30f03c6a96c7d
Eduard0803 <eduardo83204gmail.com>
2023-11-29 15:03:03 -0300

    update tests user-routes

--- gestao/tests/test_gestao.py
+++ gestao/tests/test_gestao.py
@@ -22,22 +22,6 @@ async def test_get_users(client: AsyncClient, fastapi_app: FastAPI) -> None:
 
 
 @pytest.mark.anyio
-async def test_get_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("create_user")
-    user = generate_fake_user()
-    await client.post(url, json=user)
-
-    url = fastapi_app.url_path_for("get_users")
-    response = await client.get(url)
-    user_data = response.json()[0]
-    user_id = user_data['id']
-
-    url = fastapi_app.url_path_for("get_user", user_id=user_id)
-    response = await client.get(url)
-    assert response.status_code == 200
-
-
-@pytest.mark.anyio
 async def test_get_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) -> None:
     url = fastapi_app.url_path_for("get_user", user_id=str(uuid4()))
     response = await client.get(url)
@@ -56,26 +40,9 @@ async def test_create_user_correct(client: AsyncClient, fastapi_app: FastAPI) ->
 async def test_create_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) -> None:
     url = fastapi_app.url_path_for("create_user")
     user = generate_fake_user()
-    user.pop('name')
+    user.pop("name")
     response = await client.post(url, json=user)
     assert response.status_code == 422
-
-
-@pytest.mark.anyio
-async def test_update_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("create_user")
-    user = generate_fake_user()
-    await client.post(url, json=user)
-
-    url = fastapi_app.url_path_for("get_users")
-    response = await client.get(url)
-    user_data = response.json()[0]
-    user_id = user_data['id']
-    user_data['name'] = 'joao'
-
-    url = fastapi_app.url_path_for("update_user", user_id=user_id)
-    response = await client.put(url, json=user_data)
-    assert response.status_code == 200
 
 
 @pytest.mark.anyio
@@ -86,58 +53,12 @@ async def test_update_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) 
 
 
 @pytest.mark.anyio
-async def test_delete_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("create_user")
-    user = generate_fake_user()
-    await client.post(url, json=user)
-
-    url = fastapi_app.url_path_for("get_users")
-    response = await client.get(url)
-    user_data = response.json()[0]
-    user_id = user_data['id']
-
-    url = fastapi_app.url_path_for("delete_user", user_id=user_id)
-    response = await client.delete(url)
-    assert response.status_code == 200
-
-
-@pytest.mark.anyio
-async def test_disable_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("create_user")
-    user = generate_fake_user()
-    await client.post(url, json=user)
-
-    url = fastapi_app.url_path_for("get_users")
-    response = await client.get(url)
-    user_data = response.json()[0]
-    user_id = user_data['id']
-
-    url = fastapi_app.url_path_for("disable_user", user_id=user_id)
-    response = await client.patch(url)
-    assert response.status_code == 200
-
-
-@pytest.mark.anyio
-async def test_disable_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) -> None:
+async def test_disable_user_incorrect(
+    client: AsyncClient, fastapi_app: FastAPI
+) -> None:
     url = fastapi_app.url_path_for("disable_user", user_id=str(uuid4()))
     response = await client.patch(url)
     assert response.status_code == 404
-
-
-@pytest.mark.anyio
-async def test_enable_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("create_user")
-    user = generate_fake_user()
-    await client.post(url, json=user)
-
-    url = fastapi_app.url_path_for("get_users")
-    response = await client.get(url)
-    user_data = response.json()[0]
-    user_id = user_data['id']
-
-    url = fastapi_app.url_path_for("enable_user", user_id=user_id)
-    response = await client.patch(url)
-    assert response.status_code == 200
 
 
 @pytest.mark.anyio
