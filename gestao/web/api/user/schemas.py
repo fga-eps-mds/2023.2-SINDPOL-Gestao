f00c1089601d8fa4ecaf3f98d41ca157ecaac9cc
CADU110 <cadumotaalves@gmail.com>
2023-11-30 00:44:58 -0300

    update

--- gestao/web/api/user/schemas.py
+++ gestao/web/api/user/schemas.py
@@ -3,7 +3,6 @@ from typing import List, Optional
 
 from pydantic import BaseModel
 
-
 class CreateUserDependentDTO(BaseModel):
     name: str
     birth_date: date
@@ -13,74 +12,69 @@ class CreateUserDependentDTO(BaseModel):
     phone: str
     pensioner: Optional[str]
 
-
 class CreateUserDTO(BaseModel):
-    name: str
-    address: str
-    neighborhood: str
-    city: str
-    state: str
-    zipcode: str
-    cpf: str
-    rg: str
-    birth_date: date
-    place_of_birth: str
-    blood_type: str
-    gender: str
-    father_name: str
-    mother_name: str
-    position: str
-    occupancy: str
-    admission_date: date
-    situation: str
-    phone: str
-    email: str
-    marital_status: str
-    education: str
+    fullName: str
+    warName: Optional[str]
     registration: str
-    role: str
-    category: str
-    pattern: str
-    dispatcher: str
-    dispatched_date: date
-    dependents: Optional[List[CreateUserDependentDTO]]
-    workstation: Optional[str]
-    nickname: Optional[str]
-    password: str
+    birthDate: date
+    rg: str
+    cpf: str
+    placeOfBirth: str
+    ufNatural: str
+    civilState: Optional[str]
+    cep: str
+    address: str
+    number: Optional[str]
+    city: str
+    uf: str
+    complement: Optional[str]
+    email: str
+    cellphone: str
+    phone: Optional[str]
+    gender: str
+    motherName: str
+    fatherName: Optional[str]
+    scolarity: Optional[str]
     religion: Optional[str]
-
+    bloodType: Optional[str]
+    function: str
+    actualWorkSituation: str
+    admissionDate: date
+    bodyOfLaw: str
+    lotation: Optional[str]
+    workPost: Optional[str]
+    systemRole: Optional[str]
+    dependents: Optional[List[CreateUserDependentDTO]]
 
 class UpdateUserDTO(BaseModel):
-    name: Optional[str]
-    address: Optional[str]
-    neighborhood: Optional[str]
-    city: Optional[str]
-    state: Optional[str]
-    zipcode: Optional[str]
-    cpf: Optional[str]
-    rg: Optional[str]
-    birth_date: Optional[date]
-    place_of_birth: Optional[str]
-    blood_type: Optional[str]
-    gender: Optional[str]
-    father_name: Optional[str]
-    mother_name: Optional[str]
-    position: Optional[str]
-    occupancy: Optional[str]
-    admission_date: Optional[date]
-    situation: Optional[str]
-    phone: Optional[str]
-    email: Optional[str]
-    marital_status: Optional[str]
-    education: Optional[str]
+    fullName: Optional[str]
+    warName: Optional[str]
     registration: Optional[str]
-    role: Optional[str]
-    category: Optional[str]
-    pattern: Optional[str]
-    dispatcher: Optional[str]
-    dispatched_date: Optional[date]
-    workstation: Optional[str]
-    nickname: Optional[str]
-    status: Optional[str]
-    password: Optional[str]
+    birthDate: Optional[date]
+    rg: Optional[str]
+    cpf: Optional[str]
+    placeOfBirth: Optional[str]
+    ufNatural: Optional[str]
+    civilState: Optional[str]
+    cep: Optional[str]
+    address: Optional[str]
+    number: Optional[str]
+    city: Optional[str]
+    uf: Optional[str]
+    complement: Optional[str]
+    email: Optional[str]
+    cellphone: Optional[str]
+    phone: Optional[str]
+    gender: Optional[str]
+    motherName: Optional[str]
+    fatherName: Optional[str]
+    scolarity: Optional[str]
     religion: Optional[str]
+    bloodType: Optional[str]
+    function: Optional[str]
+    actualWorkSituation: Optional[str]
+    admissionDate: Optional[date]
+    bodyOfLaw: Optional[str]
+    lotation: Optional[str]
+    workPost: Optional[str]
+    systemRole: Optional[str]
