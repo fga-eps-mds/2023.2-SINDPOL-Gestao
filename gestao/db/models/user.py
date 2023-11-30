2893632dcedc762e7898d58095fc9c8120f605ef
CADU110 <cadumotaalves@gmail.com>
2023-11-30 00:29:26 -0300

    add user model

--- gestao/db/models/user.py
+++ gestao/db/models/user.py
@@ -1,49 +1,47 @@
 from datetime import date, datetime
 from typing import Optional
-
-import ormar
-
 from gestao.db.base import BaseMeta
-
+import ormar
 
 class User(ormar.Model):
     class Meta(BaseMeta):
         tablename = "user"
 
     id: str = ormar.String(max_length=200, primary_key=True)
-    name: str = ormar.String(max_length=200)
-    address: str = ormar.String(max_length=200)
-    neighborhood: str = ormar.String(max_length=200)
-    city: str = ormar.String(max_length=100)
-    state: str = ormar.String(max_length=100)
-    zipcode: str = ormar.String(max_length=100)
-    cpf: str = ormar.String(max_length=200, unique=True)
-    rg: str = ormar.String(max_length=200, unique=True)
-    birth_date: date = ormar.Date()
-    place_of_birth: str = ormar.String(max_length=200)
-    blood_type: str = ormar.String(max_length=200)
-    gender: str = ormar.String(max_length=200)
-    father_name: str = ormar.String(max_length=200)
-    mother_name: str = ormar.String(max_length=200)
-    position: str = ormar.String(max_length=200)
-    occupancy: str = ormar.String(max_length=200)
-    admission_date: date = ormar.Date()
-    situation: str = ormar.String(max_length=200)
-    phone: str = ormar.String(max_length=200)
-    email: str = ormar.String(max_length=200)
-    marital_status: str = ormar.String(max_length=200)
-    education: str = ormar.String(max_length=200)
+    fullName: str = ormar.String(max_length=200)
+    warName: str = ormar.String(max_length=200, nullable=True)
     registration: str = ormar.String(max_length=200, unique=True)
-    role: str = ormar.String(max_length=200)
-    category: str = ormar.String(max_length=200)
-    pattern: str = ormar.String(max_length=200)
-    status: str = ormar.String(max_length=200, default="active")
-    workstation: Optional[str] = ormar.String(max_length=200, nullable=True)
-    nickname: Optional[str] = ormar.String(max_length=200, unique=True, nullable=True)
+    birthDate: date = ormar.Date()
+    rg: str = ormar.String(max_length=200, unique=True)
+    cpf: str = ormar.String(max_length=200, unique=True)
+    placeOfBirth: str = ormar.String(max_length=200)
+    ufNatural: str = ormar.String(max_length=100)
+    civilState: str = ormar.String(max_length=200, nullable=True)
+    cep: str = ormar.String(max_length=100)
+    address: str = ormar.String(max_length=200)
+    number: str = ormar.String(max_length=100, nullable=True)
+    neighborhood: str = ormar.String(max_length=100, nullable=True)
+    city: str = ormar.String(max_length=100)
+    complement: str = ormar.String(max_length=200, nullable=True)
+    uf: str = ormar.String(max_length=100)
+    email: str = ormar.String(max_length=200)
+    cellphone: str = ormar.String(max_length=200)
+    phone: str = ormar.String(max_length=200, nullable=True)
+    gender: str = ormar.String(max_length=200)
+    motherName: str = ormar.String(max_length=200)
+    fatherName: str = ormar.String(max_length=200, nullable=True)
+    scolarity: str = ormar.String(max_length=200, nullable=True)
+    religion: str = ormar.String(max_length=200, nullable=True)
+    bloodType: str = ormar.String(max_length=200, nullable=True)
+    actualWorkSituation: str = ormar.String(max_length=200)
+    admissionDate: date = ormar.Date()
+    jobRole: str = ormar.String(max_length=200, nullable=True)
+    bodyOfLaw: str = ormar.String(max_length=200)
+    lotation: str = ormar.String(max_length=200, nullable=True)
+    workPost: str = ormar.String(max_length=200, nullable=True)
+    systemRole: str = ormar.String(max_length=200, nullable=True)
     password: str = ormar.String(max_length=200, nullable=True)
-    religion: Optional[str] = ormar.String(max_length=200, nullable=True)
-    dispatcher: str = ormar.String(max_length=200)
-    dispatched_date: date = ormar.Date()
+    status: str = ormar.String(max_length=200, default="active")
     created_at: datetime = ormar.DateTime(timezone=True, default=datetime.now)
     updated_at: datetime = ormar.DateTime(
         timezone=True,
