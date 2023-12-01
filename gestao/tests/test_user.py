ac11d2edf90350f7bd7581d33a2eaeb3b786fe9f
Eduard0803 <eduardo83204gmail.com>
2023-11-30 23:14:17 -0300

    improvement: refactor test file
    Co-Authored-by: Alexandre Lema <ubc183@gmail.com>

--- gestao/tests/test_user.py
+++ gestao/tests/test_user.py
@@ -67,7 +67,7 @@ async def test_update_user_correct(client: AsyncClient, fastapi_app: FastAPI) ->
     response = await client.put(
         url,
         json={
-            "name": "joao",
+            "name": "pedro",
         },
     )
     assert response.status_code == 200
@@ -100,41 +100,46 @@ async def test_delete_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) 
     response = await client.delete(url)
     assert response.status_code == 200
 
+
 @pytest.mark.anyio
 async def test_disable_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-     url = fastapi_app.url_path_for("create_user")
-     user = generate_fake_user()
-     response = await client.post(url, json = user)
-     assert response.status_code == 200
-     user_Alexandre=response.json()
-     var = user_Alexandre["id"]
+    url = fastapi_app.url_path_for("create_user")
+    user = generate_fake_user()
+    response = await client.post(url, json=user)
+    assert response.status_code == 200
+    user_Alexandre = response.json()
+    var = user_Alexandre["id"]
 
-url = fastapi_app.url_path_for("disable_user", user_id=var)
-response = await client.patch(url)
-assert response.status_code == 200
+    url = fastapi_app.url_path_for("disable_user", user_id=var)
+    response = await client.patch(url)
+    assert response.status_code == 200
+
 
 @pytest.mark.anyio
 async def test_enable_user_correct(client: AsyncClient, fastapi_app: FastAPI) -> None:
-     url = fastapi_app.url_path_for("create_user")
-     user = generate_fake_user()
-     response = await client.post(url, json = user)
-     assert response.status_code == 200
-     user_Alexandre=response.json()
-     var = user_Alexandre["id"]
+    url = fastapi_app.url_path_for("create_user")
+    user = generate_fake_user()
+    response = await client.post(url, json=user)
+    assert response.status_code == 200
+    user_Alexandre = response.json()
+    var = user_Alexandre["id"]
 
-url = fastapi_app.url_path_for("enable_user", user_id=var)
-response = await client.patch(url)
-assert response.status_code == 200
+    url = fastapi_app.url_path_for("enable_user", user_id=var)
+    response = await client.patch(url)
+    assert response.status_code == 200
 
 
-@pytest.mark.anyio 
-async def test_disable_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("disable_user", user_id = str(uuid4()))
+@pytest.mark.anyio
+async def test_disable_user_incorrect(
+    client: AsyncClient, fastapi_app: FastAPI
+) -> None:
+    url = fastapi_app.url_path_for("disable_user", user_id=str(uuid4()))
     response = await client.patch(url)
     assert response.status_code == 404
 
+
 @pytest.mark.anyio
 async def test_enable_user_incorrect(client: AsyncClient, fastapi_app: FastAPI) -> None:
-    url = fastapi_app.url_path_for("enable_user", user_id = str(uuid4()))
+    url = fastapi_app.url_path_for("enable_user", user_id=str(uuid4()))
     response = await client.patch(url)
     assert response.status_code == 404
